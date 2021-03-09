53926f89d5123791d056c7f11b570cebf038f2ec
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-03-08 16:15:05 -0800

    fixed prepend

--- js/index.js
+++ js/index.js
@@ -71,17 +71,18 @@ links[5].style.color = 'green'
 
 
 
-// const extra = document.createElement('a')
-// extra.textContent = "J"
-// extra.href = "#"
-// extra.style.color = 'green'
-// document.querySelector('nav').prependChild(extra)
+const extra = document.createElement('a')
+extra.textContent = "Last is First"
+extra.href = "#"
+extra.style.color = 'green'
+document.querySelector('nav').prepend(extra)
 
 const extra2 = document.createElement('a')
 extra2.textContent = "James"
 extra2.href = "#"
 extra2.style.color = 'green'
 document.querySelector('nav').appendChild(extra2)
+
 
 
 const cta = document.querySelector('h1');
